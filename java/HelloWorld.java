515960c71d21374b21fdf612a90b75203636a778
yoajeetyo <88019151+yoajeetyo@users.noreply.github.com>
2021-08-18 12:20:57 +0530

    Update HelloWorld.java

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello Java edited in github");
+		System.out.println("Hello Java edited in github conflict by github");
 	}
 }
