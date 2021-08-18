d526b71b4d8752a3e1feb9b8bfb6132c77423f29
yoajeetyo <88019151+yoajeetyo@users.noreply.github.com>
2021-08-18 13:05:13 +0530

    Update HelloWorld.java

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld {
 	public static void main(String[] args){
 
 		System.out.println("Hello Java edited in github conflict by local repo");
-		System.out.println("Hello Java edited in github conflict by github");
+		System.out.println("Hello Java edited in github conflict by github changed again in master remote");
 	}
 }
