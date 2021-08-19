9fe9da312d1308929c306a4f895f490230235f8a
yoajeetyo <sajeetkumar40@gmail.com>
2021-08-20 01:23:14 +0530

    Added : Employee wage using switch case

---
+++ java/EmployeeWageBuilderSwitch.java
@@ -0,0 +1,23 @@
+public class EmployeeWageBuilderSwitch {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+
+	public static void main(String[] args) {
+		int empHrs=0;
+		int empWage=0;
+		int employeeCheck =(int) Math.floor(Math.random() * 10) % 3;
+		switch (employeeCheck) {
+			case IS_FULL_TIME :
+				empHrs=8;
+				break;
+			case IS_PART_TIME :
+				empHrs=4;
+				break;
+			default :
+				empHrs=0;
+		}
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage is : "+empWage );
+	}
+}
